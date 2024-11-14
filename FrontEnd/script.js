ad2b1dbb379ac1653343e73a6b22888e5610d8da
jbecker <becker6667@gmail.com>
2024-11-14 09:24:56 -0300

    CM - Criação tela de login e cadastro

--- FrontEnd/script.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.login-form').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    window.location.href = 'home.html'; // Redireciona para home.html
-});
