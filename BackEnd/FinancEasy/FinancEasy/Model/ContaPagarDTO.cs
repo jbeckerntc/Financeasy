90b08fb6a478bfb755c758da61c04a3e0e89d365
PedroVisconti <pedroarthurvisconti@hotmail.com>
2024-11-15 23:25:24 -0300

    Criado funções da classe ContaPagarController

---
+++ BackEnd/FinancEasy/FinancEasy/Model/ContaPagarDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace FinancEasy.Model
+{
+    public class ContaPagarDTO
+    {
+        public int IdContasPagar { get; set; }
+        public int IdCategoriaDocumento { get; set; }
+        public int IdUsuario { get; set; }
+        public string Numero { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public DateTime DataAbertura { get; set; }
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
